4c5bcf9dbeb0289213856832144fdd5231032a44
Brian James <brianpja@gmail.com>
2017-10-27 13:03:20 -0700

    styles rendering in post

--- routes/posts.js
+++ routes/posts.js
@@ -8,15 +8,45 @@ const jwt = require('jsonwebtoken');
 const boom = require('boom');
 
 router.get('/posts', (req, res, next) => {
-  knex('posts')
-    .innerJoin('users', 'posts.user_id', 'users.id')
-    .select('posts.id as post_id', 'posts.location as post_location', 'content', 'created_at', 'updated_at', 'first_name', 'last_name', 'small_img', 'year', 'user_id')
-    .then((posts) =>{
-      res.send(posts)
-    })
-    .catch((err) => {
-      next(err);
-    })
+
+  return knex.schema.raw(
+    `SELECT posts.id as post_id,
+      posts.location as post_location,
+      content,
+      created_at,
+      updated_at,
+      first_name,
+      last_name,
+      small_img,
+      year,
+      user_id,
+      (SELECT json_agg(styles)
+        FROM (
+          SELECT name
+          FROM styles
+          INNER JOIN posts_styles ON styles.id = posts_styles.style_id
+          WHERE posts_styles.post_id = posts.id
+        ) styles) as styles,
+      (SELECT json_agg(comments)
+        FROM (
+          SELECT content,
+          created_at,
+          first_name,
+          last_name,
+          small_img
+          FROM comments
+          INNER JOIN users ON users.id = comments.user_id
+          WHERE posts.id = comments.post_id
+        ) comments) as comments
+    FROM posts
+    INNER JOIN users ON posts.user_id = users.id`)
+      .then(function(posts) {
+        res.send(posts.rows)
+      })
+
+      .catch((err) => {
+        next(err);
+      })
 })
 
 
